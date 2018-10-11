b531901a635757d7db4e4e2714626721de166a4f
fanzhiwei <fan_duanlingxue@163.com.com>
2018-10-11 09:22:58 +0800

    git

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
-
+var path = require('path')
+var filePath = path.resolve();
 // 模板处理
 var  ejs = require('ejs');
 var path = require('path');
@@ -24,9 +25,16 @@ const fs= require("fs");
 app.post('/api/upload',multipartMiddleware,function (req, res) {  
   let filePath = req.files.file.path;
   let fileName = req.files.file.name;
+  let fileSize = req.files.file.size;
+  if(!fileSize){
+    res.send({
+      message:'文件无内容'
+    })
+    return
+  }
   fs.readdir('/nodeUpload',(err,data)=>{
     if (err){
-      console.log(err);
+      console.error(err);
     }else{
       data.forEach((item)=>{
         if(item==fileName){
@@ -35,6 +43,9 @@ app.post('/api/upload',multipartMiddleware,function (req, res) {
           })
         }
       })
+      fs.readFile(filePath, 'utf8', function(err, data){
+        console.log(data);  
+    });
       fs.rename(filePath,fileName,(err)=>{
         if (err) throw err
         res.send({
@@ -45,6 +56,21 @@ app.post('/api/upload',multipartMiddleware,function (req, res) {
   })
 });
 
+// 文件夹内文件目录
+let filearr = []
+app.get('/api/getdir',function(req,res){
+  fs.readdir(filePath,(err,files)=>{
+    if(err)console.error(err)
+    files.forEach((file)=>{
+      fs.stat(path.join(filePath,file),(err,stats)=>{
+        if(stats.isFile()){
+          filearr.push(path.join(filePath,file));
+          writeFile(filearr)
+        }
+      })
+    })
+  })
+})
 app.get('/api/download',function(req,res){
   var url = 'G:/nodeUpload/水电.txt';
   url = path.resolve(url);
@@ -52,9 +78,26 @@ app.get('/api/download',function(req,res){
   res.download(url);
 })
 
+
+//获取后缀名
+function getdir(url){
+  var arr = url.split('.');
+  var len = arr.length;
+  return arr[len-1];
+}
+
+function writeFile(data){
+  console.log(data)
+  var data = data.join("\r\n")
+  fs.writeFile(path.join(filePath,'filelist.txt'),data,(err)=>{
+    if(err)console.log(err)
+    console.log('写入成功')
+  })
+}
+
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('', host, port);
+  console.log('欢迎来到', host, port);
 });
